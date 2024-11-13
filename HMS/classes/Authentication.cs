004369fba0799556ff564a3483b23a18c3e33db5
Rhys <Rhys@DESKTOP-JMQQDNC>
2024-11-13 10:55:02 +0000

    Created the Log File and got the initialise application running.

--- HMS/classes/Authentication.cs
+++ HMS/classes/Authentication.cs
@@ -8,6 +8,7 @@ public class Authentication
     public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
     public string Phone { get; set; }
     public string[] Address { get; set; }
     public List<Dictionary<string,string>> Notes { get; set; }
--- HMS/classes/Utility.cs
+++ HMS/classes/Utility.cs
@@ -1,9 +1,19 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
 
 namespace HMS.classes;
 
 public class Utility
 {
+    #region UtitlityVariables
+
+    private string _authFilePath = "./auth.json";
+    private string _patientDirectory = "./Patients";
+    private string _logDirectory = "./Logs";
+    // Learnt DateTime from https://www.c-sharpcorner.com/blogs/date-and-time-format-in-c-sharp-programming1
+    private string _logFilePath = $"./Logs/log_{DateTime.Now.ToString("ddMMyyyy")}.txt";
+
+    #endregion
+    
     #region InitialiseApplication
 
     /*
@@ -17,39 +27,91 @@ public class Utility
     {
         // I used W3Schools and the Microsoft Documentation to assist me with files in c#.
 
+        #region LogFile
+
+        if (!Directory.Exists(_logDirectory))
+        {
+            Directory.CreateDirectory(_logDirectory);
+        }
+
+        if (!File.Exists(_logFilePath))
+        {
+            File.Create(_logFilePath).Close();
+       
+            // File.AppendText()
+            // _logWriter = new StreamWriter(_logFilePath);
+            //
+            // _logWriter.Write($"[LOG > APP | START-UP] Created '{_logDirectory}' directory and '{_logFilePath}' file.");
+            WriteToLogFile($"[LOG > APP | START-UP] Created '{_logDirectory}' directory and '{_logFilePath}' file.");
+        }
+        
+
+        #endregion
+        
         #region PatientFolderExist
 
         // Checks to see if the patient folder exists.
-        if (!Directory.Exists("./Patients"))
+        if (!Directory.Exists(_patientDirectory))
         {
-            Console.WriteLine("[APP | Start-up] INFO > '/patients' folder doesn't exist. Creating...");
+            WriteToLogFile($"[APP | Start-up] INFO > '{_patientDirectory}' folder doesn't exist. Creating...");
             
             // Create a folder for the patient records.
-            Directory.CreateDirectory("./Patients");
+            Directory.CreateDirectory(_patientDirectory);
         }
         else
         {
-            Console.WriteLine("[APP | Start-up] INFO > '/patients' folder exists.");
+            Console.WriteLine($"[APP | Start-up] INFO > '{_patientDirectory}' folder exists.");
         }
         #endregion
 
         #region AuthFileExists
 
-        if (!File.Exists("./auth.json"))
+        if (!File.Exists(_authFilePath))
         {
-            Console.WriteLine("[APP | Start-up] INFO > '/auth.json' folder doesn't exist. Creating...");
+            WriteToLogFile($"[APP | Start-up] INFO > '{_authFilePath}' folder doesn't exist. Creating...");
             
             // Create the auth.json file
-            File.Create("./auth.json");
+            File.Create(_authFilePath).Close();
         }
         else
         {
-            Console.WriteLine("[APP | Start-up] INFO > '/auth.json' folder exists.");
+            Console.WriteLine($"[APP | Start-up] INFO > '{_authFilePath}' folder exists.");
 
+        }
+
+        #endregion
+
+        #region CheckAuthFile
+
+        if (File.ReadAllText(_authFilePath) == "")
+        {
+            WriteToLogFile($"[APP | Start-up] INFO > '{_authFilePath}' is empty. Creating new admin user...");
+            List<Authentication> userObj = new List<Authentication>();
+            
+            // Creating a default admin user
+            userObj.Add(new Authentication
+            {
+                Id = "001", Name = "Administrator", Email = "admin@admin.com", Password = "admin", Phone = "000000000",
+                Appointments = [], Address = [], Notes = []
+            });
+            
+            // Serialise to json
+            string jsonString = JsonConvert.SerializeObject(userObj, Formatting.Indented);
+            File.WriteAllText(_authFilePath, jsonString);
+        }
+        else
+        {
+            WriteToLogFile($"[APP | Start-up] INFO > '{_authFilePath}' has content.");
         }
 
         #endregion
     }
 
     #endregion
+
+    void WriteToLogFile(string data)
+    {
+        File.AppendAllText(_logFilePath, data + '\n');
+    }
+    
 }
